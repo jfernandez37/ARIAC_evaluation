809adde371460496499587e19390bc4694eefd58
Justin Albrecht <jalbrecht23@gmail.com>
2024-05-10 11:28:26 -0400

    handle case where no run was successful for a given team

--- automated_evaluation/scoring/structs.py
+++ automated_evaluation/scoring/structs.py
@@ -17,7 +17,7 @@ class TeamSubmission():
         self.order_submissions = order_submissions
         
 class TrialInfo():
-    def __init__(self, trial_name: str, trial_scores: dict[str,float], team_submissions = dict[str, TeamSubmission]):
+    def __init__(self, trial_name: str, trial_scores: dict[str,float], team_submissions: dict[str, TeamSubmission]):
         self.trial_name = trial_name
         self.trial_scores = trial_scores
         self.team_submissions = team_submissions
